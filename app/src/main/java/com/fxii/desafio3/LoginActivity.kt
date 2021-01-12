7b374051b9dfcb5552c2c5b992af6992f1d63a01
Fabricio Kassardjian <fkay13@gmail.com>
2021-01-11 23:49:45 -0300

    Tela de registro

--- app/src/main/java/com/fxii/desafio3/LoginActivity.kt
+++ app/src/main/java/com/fxii/desafio3/LoginActivity.kt
@@ -1,11 +1,27 @@
 package com.fxii.desafio3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fxii.desafio3.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupObservables()
+    }
+
+    private fun setupObservables() {
+        with(binding) {
+            btnLoginRegistrar.setOnClickListener{
+                var intent = Intent(this@LoginActivity, RegistrarActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/fxii/desafio3/RegistrarActivity.kt
@@ -0,0 +1,25 @@
+package com.fxii.desafio3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fxii.desafio3.databinding.ActivityRegistrarBinding
+
+class RegistrarActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegistrarBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegistrarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupObservables()
+    }
+
+    private fun setupObservables() {
+        with(binding) {
+            btnRegistrarVoltar.setOnClickListener{
+                finish()
+            }
+        }
+    }
+}
